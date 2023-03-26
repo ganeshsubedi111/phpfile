f1d3e7034f530b36a9229fe6b9030797fe17b0ce
Ganesh subedi <73029563+ganeshsubedi111@users.noreply.github.com>
2023-03-26 22:25:21 +0545

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,63 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['password']) &&
+        isset($_POST['gender']) && isset($_POST['email']) &&
+        isset($_POST['phoneCode']) && isset($_POST['phone'])) {
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $phoneCode = $_POST['phoneCode'];
+        $phone = $_POST['phone'];
+
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "test";
+
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+
+            if ($rnum == 0) {
+                $stmt->close();
+
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$username, $password, $gender, $email, $phoneCode, $phone);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
